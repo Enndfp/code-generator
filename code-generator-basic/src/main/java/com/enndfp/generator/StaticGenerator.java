8a7e6a47bb114bdcfb96985fa52d5125e592e59f
Enndfp <3193947435@qq.com>
2024-02-03 16:06:39 +0800

    feat: ✨ 静态文件生成

---
+++ code-generator-basic/src/main/java/com/enndfp/generator/StaticGenerator.java
@@ -0,0 +1,94 @@
+package com.enndfp.generator;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.ArrayUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * 静态文件生成器
+ *
+ * @author Enndfp
+ */
+public class StaticGenerator {
+
+    public static void main(String[] args) {
+        // 获取项目路径
+        String projectPath = System.getProperty("user.dir");
+        // 获取项目的父级目录
+        File parentFile = new File(projectPath).getParentFile();
+        // 输入路径
+        String inputPath = new File(parentFile, "code-generator-demo-projects" + File.separator + "acm-template").getAbsolutePath();
+        // 输出路径
+        String outputPath = projectPath;
+        copyFilesByRecursive(inputPath, outputPath);
+    }
+
+    /**
+     * 拷贝文件（Hutool 实现，会将输入目录完整拷贝到输出目录下）
+     *
+     * @param inputPath  输入路径
+     * @param outputPath 输出路径
+     */
+    public static void copyFilesByHutool(String inputPath, String outputPath) {
+        FileUtil.copy(inputPath, outputPath, false);
+    }
+
+    /**
+     * 递归拷贝文件（递归实现，会将输入目录完整拷贝到输出目录下）
+     *
+     * @param inputPath  输入路径
+     * @param outputPath 输出路径
+     */
+    public static void copyFilesByRecursive(String inputPath, String outputPath) {
+        File inputFile = new File(inputPath);
+        File outputFile = new File(outputPath);
+        try {
+            copyFileByRecursive(inputFile, outputFile);
+        } catch (Exception e) {
+            System.err.println("文件复制失败");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 文件 A => 目录 B，则文件 A 放在目录 B 下
+     * 文件 A => 文件 B，则文件 A 覆盖文件 B
+     * 目录 A => 目录 B，则目录 A 放在目录 B 下
+     * <p>
+     * 核心思路：先创建目录，然后遍历目录内的文件，依次复制
+     *
+     * @param inputFile  输入文件
+     * @param outputFile 输出文件
+     * @throws IOException IO异常
+     */
+    private static void copyFileByRecursive(File inputFile, File outputFile) throws IOException {
+        // 区分是文件还是目录
+        if (inputFile.isDirectory()) {
+            File destOutputFile = new File(outputFile, inputFile.getName());
+            // 目标目录不存在，创建目录
+            if (!destOutputFile.exists()) {
+                destOutputFile.mkdirs();
+            }
+            // 获取目录下的所有文件和子目录
+            File[] files = inputFile.listFiles();
+            // 无子文件，直接结束
+            if (ArrayUtil.isEmpty(files)) {
+                return;
+            }
+            for (File file : files) {
+                // 递归拷贝下一层文件
+                copyFileByRecursive(file, destOutputFile);
+            }
+        } else {
+            // 是文件，直接复制到目标目录下
+            Path destPath = outputFile.toPath().resolve(inputFile.getName());
+            Files.copy(inputFile.toPath(), destPath, StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+}
