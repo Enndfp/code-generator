5680dcce5feefd53e9925e01d555cb40ec2d4b61
Enndfp <3193947435@qq.com>
2024-02-04 16:43:35 +0800

    feat: ✨ 引入 maven 插件进行 jar 包构建

--- code-generator-basic/src/main/java/com/enndfp/Main.java
+++ code-generator-basic/src/main/java/com/enndfp/Main.java
@@ -7,9 +7,6 @@ import com.enndfp.cli.CommandExecutor;
  */
 public class Main {
     public static void main(String[] args) {
-//        args = new String[]{"generate", "-l", "-a", "-o"};
-//        args = new String[]{"config"};
-        args = new String[]{"list"};
         CommandExecutor commandExecutor = new CommandExecutor();
         commandExecutor.doExecute(args);
     }
